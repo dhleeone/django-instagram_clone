48b1677496b13351dc32b5bfec7345ca12314be6
DH Lee <78197111+dhleeone@users.noreply.github.com>
2022-01-03 16:04:05 +0900

    Update settings.py

--- leestagram/settings.py
+++ leestagram/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2tz_m=48q-$q1t$_rb!6!1+81i2st7=fd5sl6hd0o80gfct1vr'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
